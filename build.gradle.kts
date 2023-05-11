39c506ef79004e930cb08c44af8d27afe6cc03eb
infernostars <blazem7708.2@gmail.com>
2023-05-11 14:20:21 -0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("js") version "1.8.0"
+}
+
+group = "al.twowcentr"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.2.0-pre.346")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.2.0-pre.346")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.9.3-pre.346")
+}
+
+kotlin {
+    js {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport {
+                    enabled.set(true)
+                }
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "twowcentral-website"
+
